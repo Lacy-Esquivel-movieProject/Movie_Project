c4a530fee9fc7f52221d411aecaec3bf5a6ba082
Steven Lacy <steven.lacy11@gmail.com>
2021-10-18 15:10:17 -0500

    working on imgs

--- movie_Project.js
+++ movie_Project.js
@@ -5,6 +5,7 @@ function getMovies() {
 	return fetch("https://jazzy-believed-foxglove.glitch.me/movies").then((response) => response.json());//response.json() returns a promise
 }
 
+let stars = new Image()
 
 
 $(document).ready(function () {
