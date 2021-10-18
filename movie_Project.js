9cff976d44334f293d56539099fe6653300baa4e
Steven Lacy <steven.lacy11@gmail.com>
2021-10-18 14:57:02 -0500

    Movies set up

--- movie_Project.js
+++ movie_Project.js
@@ -2,26 +2,24 @@
 
 
 function getMovies() {
-	return fetch(`https://jazzy-believed-foxglove.glitch.me/movies`).then((response) => response.json());//response.json() returns a promise
+	return fetch("https://jazzy-believed-foxglove.glitch.me/movies").then((response) => response.json());//response.json() returns a promise
+}
 
-	}
+
 
 $(document).ready(function () {
 
+	getMovies().then(movies => {
+		console.log(movies);
+		movies.forEach(function (movie) {
+			console.log(movie)
+			$("#moviesContainer").append("<h1>" + movie.title + "</h1>" + "<h4>" + "<em>" + "Directed by: " + movie.director + "</em>" + "</h4>" + "Poster" + "<br>" + "Year released: " + movie.year + "<br>" + "Rating: " + movie.rating + " stars" + "<br>" + movie.genre + "<br>" + "<p>" + "<strong>" + movie.plot + "</strong>" + "</p>" + "<hr>")
 
 
-		getMovies().then(movies => {
-			console.log(movies);
-			movies.forEach(function(movie){
-				console.log(movie)
-				$("#moviesContainer").append("<h1>" + movie.title + "</h1>"
-					+ "<span>" + img + "</span><br>")
-
-				let img = 'img src="https://jazzy-believed-foxglove.glitch.me/movies' + movie.poster + '.png"'
-			} )
+		})
 
 
-		});
+	});
 
 
 	$("#refresh").click(function () {
