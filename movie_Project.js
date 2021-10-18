b43582e9b6daab501ec30083c4bfc280039df1e0
Bernie Esquivel <bernie.esquivel01@gmail.com>
2021-10-17 19:32:20 -0500

    movie_project loading page

--- movie_Project.js
+++ movie_Project.js
@@ -3,11 +3,13 @@
 $(document).ready(function () {
 
 
-
 	function getMovies() {
 		return fetch(`https://jazzy-believed-foxglove.glitch.me/movies`).then((response) => response.json());//response.json() returns a promise
 	}
 
 getMovies().then(movies => console.log(movies));
+
+
+	const addMovie = (title,rating,id) => movies.push({"title":title,"rating":rating, "id":id});
 
 })
